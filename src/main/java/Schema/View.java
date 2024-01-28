670fd15f762b33322a635e91fbfde0c84e4af664
pooyan <thepooyan@gmail.com>
2024-01-28 13:30:52 +0330

    first java view for test

--- src/main/java/Schema/View.java
+++ src/main/java/Schema/View.java
@@ -5,7 +5,6 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
 public class View {
     public View(String storyNote, Point2D coordinates, List<Direction> neighbours) {
         this.storyNote = storyNote;
@@ -18,4 +17,32 @@ public class View {
     private Object[] objects;
     private String hiddenNote = "nothing!";
     private List<Direction> neighbours = new ArrayList<>();
+
+    public String getStoryNote() {
+        return storyNote;
+    }
+
+    public Point2D getCoordinates() {
+        return coordinates;
+    }
+
+    public Object[] getObjects() {
+        return objects;
+    }
+
+    public String getHiddenNote() {
+        return hiddenNote;
+    }
+
+    public List<Direction> getNeighbours() {
+        return neighbours;
+    }
+
+    public void setObjects(Object[] objects) {
+        this.objects = objects;
+    }
+
+    public void setHiddenNote(String hiddenNote) {
+        this.hiddenNote = hiddenNote;
+    }
 }
---
+++ src/main/java/Views/V00.java
@@ -0,0 +1,23 @@
+package Views;
+
+import Actions.Direction;
+import Objects.Box;
+import Schema.Object;
+import Schema.View;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class V00 extends View {
+    public V00() {
+        super(
+                "this is story note, there's a box here on the table.",
+            new Point2D.Double(0,0),
+            new ArrayList<>(List.of(Direction.west, Direction.south))
+        );
+        Object[] a = {new Box()};
+        this.setObjects(a);
+        this.setHiddenNote("there is this code written under table: 663");
+    }
+}
