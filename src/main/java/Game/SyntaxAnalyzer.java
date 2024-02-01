3275a1853e2a053aaa92a73decb5b8e2af7791cb
pooyan <thepooyan@gmail.com>
2024-02-02 01:55:49 +0330

    syntax accepts actions that exist but are not redefined in dictionary

--- src/main/java/Game/SyntaxAnalyzer.java
+++ src/main/java/Game/SyntaxAnalyzer.java
@@ -12,30 +12,46 @@ public class SyntaxAnalyzer {
        if (response.trim().equals("")) return new Idol();
 
        String[] chunks = response.split(" ");
+       Optional<Action> optionalAction = searchActions(toCamelCase(chunks[0]));
+
+
+       if (optionalAction.isPresent()) {
+           Action action = optionalAction.get();
+                if (chunks.length > 1) {
+                    String[] newArray = Arrays.copyOfRange(chunks, 1, chunks.length);
+                    action.setChunks(newArray);
+                }
+           return action;
+       }
+
        Optional<String> actionLabel = dictionary.getActionLabel(chunks[0]);
 
        if (actionLabel.isEmpty()) {
            return new Unkown(chunks[0]);
        }
 
+       Action action = searchActions(actionLabel.get()).get();
+        if (chunks.length > 1) {
+            String[] newArray = Arrays.copyOfRange(chunks, 1, chunks.length);
+            action.setChunks(newArray);
+        }
+
+       return action;
+
+    }
+    public Optional<Action> searchActions(String actionLabel) {
         try {
-            Class<?> clazz = Class.forName("Actions." + actionLabel.get());
+            Class<?> clazz = Class.forName("Actions." + actionLabel);
             Object instance = clazz.getDeclaredConstructor().newInstance();
 
             if (instance instanceof Action action) {
-
-                if (chunks.length > 1) {
-                    String[] newArray = Arrays.copyOfRange(chunks, 1, chunks.length);
-                    action.setChunks(newArray);
-                }
-
-                return action;
+                return Optional.of(action);
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return new Idol();
+        } catch (Exception ignored) {}
+        return Optional.empty();
+    }
+    public String toCamelCase(String string) {
+        string = string.toLowerCase();
+        return string.substring(0,1).toUpperCase().concat(string.substring(1));
     }
 }
