b46d7ae68715069fc38bc24a570906205099f0af
pooyan <thepooyan@gmail.com>
2024-02-02 02:09:21 +0330

    syntax analyzer update

--- src/main/java/Game/SyntaxAnalyzer.java
+++ src/main/java/Game/SyntaxAnalyzer.java
@@ -8,36 +8,26 @@ import java.util.Optional;
 public class SyntaxAnalyzer {
     Dictionary dictionary = new Dictionary();
     public Action analyzeResponse(String response) {
+
        response = response.toLowerCase();
        if (response.trim().equals("")) return new Idol();
 
        String[] chunks = response.split(" ");
        Optional<Action> optionalAction = searchActions(toCamelCase(chunks[0]));
 
-
        if (optionalAction.isPresent()) {
            Action action = optionalAction.get();
-                if (chunks.length > 1) {
-                    String[] newArray = Arrays.copyOfRange(chunks, 1, chunks.length);
-                    action.setChunks(newArray);
-                }
-           return action;
+           return prepareAction(action, chunks);
        }
 
        Optional<String> actionLabel = dictionary.getActionLabel(chunks[0]);
 
-       if (actionLabel.isEmpty()) {
-           return new Unkown(chunks[0]);
+       if (actionLabel.isPresent()) {
+           Action action = searchActions(actionLabel.get()).get();
+           return prepareAction(action, chunks);
        }
 
-       Action action = searchActions(actionLabel.get()).get();
-        if (chunks.length > 1) {
-            String[] newArray = Arrays.copyOfRange(chunks, 1, chunks.length);
-            action.setChunks(newArray);
-        }
-
-       return action;
-
+        return new Unkown(chunks[0]);
     }
     public Optional<Action> searchActions(String actionLabel) {
         try {
@@ -53,5 +43,12 @@ public class SyntaxAnalyzer {
     public String toCamelCase(String string) {
         string = string.toLowerCase();
         return string.substring(0,1).toUpperCase().concat(string.substring(1));
+    }
+    public Action prepareAction(Action action, String[] chunks) {
+        if (chunks.length > 1) {
+            String[] newArray = Arrays.copyOfRange(chunks, 1, chunks.length);
+            action.setChunks(newArray);
+        }
+        return action;
     }
 }
